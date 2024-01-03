86546617a503d64e1a8ee42cced0fb198bf88aa9
TAN Hongkui <hongkui.tan@boschrexroth.com.cn>
2024-01-03 15:15:04 +0800

    rename rokit_locator_api.py to talk_to_rokit_locator.py

--- test_api.py
+++ test_api.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from rokit_locator_api import *
+from talk_to_rokit_locator import *
 
 # get_client_localization_pose("localhost", 9011)
 get_client_control_mode("localhost", 9004)
