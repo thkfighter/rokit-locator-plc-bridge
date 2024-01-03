e33a1243e5f711b94e6d57776153f7b79f61d84c
TAN Hongkui <hongkui.tan@boschrexroth.com.cn>
2024-01-03 17:39:15 +0800

    downloading files does not fully work in install_seed_s7.sh

--- scripts/install_seed_s7.sh
+++ scripts/install_seed_s7.sh
@@ -9,18 +9,19 @@
 # Define variables
 GITEE_REPO_OWNER="thkfighter"
 GITEE_REPO_NAME="locator_plc_bridge"
-executable_name="seed_s7" # TODO system version
 EXECUTABLE_VERSION="v1.0.4"
-codename = $(lsb_release -c)
-arch = $(uname -m)
-if [[$codename == *"focal"*]] && $arch == "x86_64"; then
-    suffix = "_ubuntu20.04_amd64";
-elif [[$codename == *"jammy"*]] && $arch == "x86_64";
-    suffix = "_ubuntu22.04_amd64";
+seed_s7="seed_s7"
+codename=$(lsb_release -c)
+arch=$(uname -m)
+if echo $codename | grep -qF "focal" && [ "$arch"="x86_64" ]; then
+    suffix="_ubuntu20.04_amd64";
+    # echo $suffix;
+elif echo $codename | grep -qF "jammy" && [ "$arch"="x86_64" ]; then
+    suffix="_ubuntu22.04_amd64";
 fi
-executable_name = "$executable_name$suffix"
+seed_s7_download="$seed_s7$suffix"
 
-DOWNLOAD_URL="https://gitee.com/${GITEE_REPO_OWNER}/${GITEE_REPO_NAME}/releases/download/${EXECUTABLE_VERSION}/${EXECUTABLE_NAME}"
+DOWNLOAD_URL="https://gitee.com/${GITEE_REPO_OWNER}/${GITEE_REPO_NAME}/releases/download/${EXECUTABLE_VERSION}/${seed_s7_download}"
 
 # https://gitee.com/thkfighter/locator_plc_bridge/releases/download/v1.0.4/seed_s7_ubuntu22.04_amd64
 
@@ -29,31 +30,33 @@ EXEC_DIR="/home/${USER}/rokit/service"
 mkdir -p "${EXEC_DIR}"
 
 # Download the executable
-curl -L "${DOWNLOAD_URL}" -o "${EXEC_DIR}/${EXECUTABLE_NAME}"
-chmod +x "${EXEC_DIR}/${EXECUTABLE_NAME}"
+curl -L "${DOWNLOAD_URL}" --output ${EXEC_DIR}/${seed_s7}
+chmod +x "${EXEC_DIR}/${seed_s7}"
 
 # Create the systemd service file
-SERVICE_FILE="/etc/systemd/system/${EXECUTABLE_NAME}.service"
-cat <<EOF > "${SERVICE_FILE}"
+SERVICE_FILE="/etc/systemd/system/${seed_s7}.service"
+cat > ${SERVICE_FILE}<<-EOF 
 [Unit]
-Description=My Executable Service
+Description=ROKIT Locator pose initialization, using protocol s7 to communicate with Siemens S7-1200
 After=network.target
 
 [Service]
-ExecStart=${EXEC_DIR}/${EXECUTABLE_NAME}
-Restart=on-failure
+ExecStart=/home/$USER/rokit/service/$seed_s7 --config /home/$USER/rokit/service/seed_s7_config.json
+Restart=always
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
+cat > "${SERVICE_FILE}"
+
 # Reload systemd daemon to recognize new service file
-systemctl daemon-reload
+# systemctl daemon-reload
 
 # Enable the service to start at boot
-systemctl enable "${EXECUTABLE_NAME}.service"
+# systemctl enable "${EXECUTABLE_NAME}.service"
 
 # Start the service
 # systemctl start "${EXECUTABLE_NAME}.service"
 
-echo "Successfully set up ${EXECUTABLE_NAME} as a systemd service."
+echo "Successfully set up ${seed_s7} as a systemd service."
