573cf0ad78ac61ff6cd9017c828fb874b887cdc8
TAN Hongkui <hongkui.tan@boschrexroth.com.cn>
2024-01-04 17:41:37 +0800

    main functions of install_seed_s7.sh work; handle_locator_maps.sh works except restore_maps()

---
+++ scripts/handle_locator_maps.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+#
+# Created On: 2024-01-05
+# SPDX-FileCopyrightText: Copyright (c) 2024 Shanghai Bosch Rexroth Hydraulics & Automation Ltd.
+# SPDX-License-Identifier: MIT
+#
+
+# sudo bash script.sh
+# sudo -u $USER -E bash script.sh
+# sudo -E bash script.sh
+
+# TODO check if the script is run with sudo
+
+version=1.8 # do not include minor version number
+dir_prefix=""
+dir_recordings="/var/lib/docker/volumes/BoschRexrothLocalizationClientWorkdir/_data/${version}/client/slam/recordings"
+dir_sketches="/var/lib/docker/volumes/BoschRexrothLocalizationClientWorkdir/_data/${version}/client/slam/maps"
+dir_client_loc_maps="/var/lib/docker/volumes/BoschRexrothLocalizationClientWorkdir/_data/${version}/client/loc/maps"
+dir_server_maps="/var/lib/docker/volumes/BoschRexrothLocalizationServerWorkdir/_data/${version}/server/mus/maps"
+current_user="tao4sgh"
+dir_backup="/home/tao4sgh/rokit/maps" # TODO user
+backup_maps(){
+    # backup locator
+    mkdir -p $dir_backup
+    pushd $dir_backup
+    # recordings 激光记录
+    rsync -av --relative ${dir_recordings} $dir_backup
+    # sketches 草图
+    rsync -av --relative ${dir_sketches} $dir_backup
+    # client localization maps 客户端定位用地图
+    rsync -av --relative $dir_client_loc_maps $dir_backup
+    # server maps and updates 服务器地图和更新
+    rsync -av --relative $dir_server_maps $dir_backup
+    popd
+    chown $current_user:$current_user $dir_backup -R
+    
+}
+
+restore_maps(){
+    :
+    # 恢复备份文件的时候，修改文件权限和所有者
+    # sudo chmod 644 *
+    # sudo chown 9421:9421 *
+}
+
+list_dir(){
+    echo
+    echo $1
+    ls -lh $1
+}
+
+# list_sketches(){
+#     :
+# }
+
+# list_client_loc_maps(){
+#     :
+# }
+
+# list_server_maps(){
+#     :
+# }
+
+if ! which tree >& /dev/null
+then
+    apt install tree -y
+fi
+
+# ANSI escape codes for colors
+RED='\033[0;31m'
+COLOR="\033[43;42m"
+GREEN_B="\e[42m" # https://gist.github.com/Prakasaka/219fe5695beeb4d6311583e79933a009
+YELLOW_F='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+start_menu(){
+    # clear
+    echo
+    printf "${COLOR}====================================${NC}\n"
+    printf "${COLOR}%-10s%-26s${NC}\n" "Script" $0
+    printf "${COLOR}%-10s%-26s${NC}\n" "OS" "$(lsb_release -ds)"
+    printf "${COLOR}%-10s%-26s${NC}\n" "Author" "TAN Hongkui"
+    printf "${GREEN_B}====================================${NC}\n"
+    echo
+    printf "${YELLOW_F} 1. backup maps${NC}\n"
+    printf "${YELLOW_F} 2. restore maps${NC}\n"
+    printf "${YELLOW_F} 3. list recordings${NC}\n"
+    printf "${YELLOW_F} 4. list sketches${NC}\n"
+    printf "${YELLOW_F} 5. list client localization maps${NC}\n"
+    printf "${YELLOW_F} 6. list server maps${NC}\n"
+    printf "${YELLOW_F} 7. list maps backup${NC}\n"
+    printf " 0. ctrl+c to exit\n"
+    echo
+    
+    # read -p "ROKIT Locator version (1.6 or 1.8):" locator_version
+    read -p "enter number:" num
+    case "$num" in
+        0)
+            exit 1
+        ;;
+        1)
+            backup_maps
+        ;;
+        2)
+            restore_maps
+        ;;
+        3)
+            list_dir $dir_recordings
+        ;;
+        4)
+            list_dir $dir_sketches
+        ;;
+        5)
+            list_dir $dir_client_loc_maps
+        ;;
+        6)
+            list_dir $dir_server_maps
+        ;;
+        7)
+            tree -h $dir_backup
+        ;;
+        *)
+            # clear
+            echo -e "Please enter correct numbers"
+            sleep 2s
+            start_menu
+        ;;
+    esac
+}
+
+while true; do
+    start_menu
+    sleep 2s
+    if [ $? -ne 0 ]
+    then
+        break
+    fi
+done
--- scripts/install_seed_s7.sh
+++ scripts/install_seed_s7.sh
@@ -6,66 +6,185 @@
 # SPDX-License-Identifier: MIT
 #
 
+# sudo -u $USER -E bash scripts/install_seed_s7.sh
+# sudo -E bash scripts/install_seed_s7.sh
+
 # Define variables
 GITEE_REPO_OWNER="thkfighter"
 GITEE_REPO_NAME="locator_plc_bridge"
 EXECUTABLE_VERSION="v1.0.4"
-seed_s7="seed_s7"
-codename=$(lsb_release -c)
 arch=$(uname -m)
-if echo $codename | grep -qF "jammy"; then
-    os="ubuntu22.04";
-    elif echo $codename | grep -qF "focal"; then
-    os="ubuntu20.04";
-    elif echo $codename | grep -qF "bionic"; then
-    os="ubuntu18.04";
-fi
-if [ "$arch"="x86_64" ]; then
-    arch="amd64";
-    # elif [ "$arch"="aarch64" ]; then
-    # ;
-fi
-seed_s7_download="${seed_s7}_${os}_${arch}"
+codename=$(lsb_release -cs)
+case $codename in
+    "jammy") os="ubuntu22.04"
+    ;;
+    "focal") os="ubuntu20.04"
+    ;;
+    "bionic") os="ubuntu18.04"
+    ;;
+    *) echo "not built for this OS version"
+    ;;
+esac
 
-DOWNLOAD_URL="https://gitee.com/${GITEE_REPO_OWNER}/${GITEE_REPO_NAME}/releases/download/${EXECUTABLE_VERSION}/${seed_s7_download}"
+if [ "$arch"="x86_64" ]
+then
+    arch="amd64"
+elif [ "$arch"="aarch64" ]
+then
+    :
+fi
+
+url_prefix="https://gitee.com/${GITEE_REPO_OWNER}/${GITEE_REPO_NAME}/releases/download/${EXECUTABLE_VERSION}/"
+
 # https://gitee.com/thkfighter/locator_plc_bridge/releases/download/v1.0.4/seed_s7_ubuntu22.04_amd64
-config_url="https://gitee.com/${GITEE_REPO_OWNER}/${GITEE_REPO_NAME}/releases/download/${EXECUTABLE_VERSION}/${seed_s7}_config.json"
+# https://gitee.com/thkfighter/locator_plc_bridge/releases/download/v1.0.4/seed_s7_config.json
+
+# TODO Check whether the files have been downloaded
 
 # Create a directory to store the executable (if not exists)
 EXEC_DIR="/home/${USER}/rokit/service"
 mkdir -p "${EXEC_DIR}"
 
-# Download the executable
-curl -L "${DOWNLOAD_URL}" --output ${EXEC_DIR}/${seed_s7}
-chmod +x "${EXEC_DIR}/${seed_s7}"
-curl -L "${config_url}" --output ${EXEC_DIR}/${seed_s7}_config.json
+install_seed_s7()
+{
+    target="seed_s7_${os}_${arch}"
+    target_config="seed_s7_config.json"
+    url_target="${url_prefix}${target}"
+    # Download the executable
+    wget -nv -P ${EXEC_DIR} ${url_target}
+    chmod +x "${EXEC_DIR}/${target}"
+    wget -nv -P ${EXEC_DIR} ${url_prefix}${target_config}
+    
+    # Create the systemd service file
+    SERVICE_FILE="/etc/systemd/system/${target}.service"
+    cat > ${SERVICE_FILE} <<-EOF
+    [Unit]
+    Description=ROKIT Locator pose initialization, using protocol s7 to communicate with Siemens S7-1200
+    After=network.target
 
-# Create the systemd service file
-SERVICE_FILE="/etc/systemd/system/${seed_s7}.service"
-cat > ${SERVICE_FILE}<<-EOF
-[Unit]
-Description=ROKIT Locator pose initialization, using protocol s7 to communicate with Siemens S7-1200
-After=network.target
+    [Service]
+    ExecStartPre=/bin/sleep 3 # delay 3 seconds
+    ExecStart=/home/${USER}/rokit/service/${target} --config /home/${USER}/rokit/service/${target}_config.json
+    Restart=always
 
-[Service]
-ExecStart=/home/$USER/rokit/service/$seed_s7 --config /home/$USER/rokit/service/${seed_s7}_config.json
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
+    [Install]
+    WantedBy=multi-user.target
 EOF
+    
+    echo "======${SERVICE_FILE}"
+    cat "${SERVICE_FILE}"
+    echo "======"
+    
+    # Reload systemd daemon to recognize new service file
+    systemctl daemon-reload
+    
+    # Enable the service to start at boot
+    systemctl enable "${target}.service"
+    
+    # Start the service
+    systemctl start "${target}.service"
+    
+    echo "Successfully set up ${target} as a systemd service."
+    
+}
 
-echo "======${SERVICE_FILE}"
-cat "${SERVICE_FILE}"
-echo "======"
 
-# Reload systemd daemon to recognize new service file
-systemctl daemon-reload
+install_relay(){
+    target="relay.py"
+    url_target="https://gitee.com/${GITEE_REPO_OWNER}/${GITEE_REPO_NAME}/releases/download/${EXECUTABLE_VERSION}/${target}.py"
+    # https://gitee.com/thkfighter/locator_plc_bridge/releases/download/v1.0.4/relay.py
+    
+    # Download the executable
+    wget -nv -P ${EXEC_DIR} ${url_target}
+    chmod +x "${EXEC_DIR}/${target}"
+    
+    # Create the systemd service file
+    SERVICE_FILE="/etc/systemd/system/${target}.service"
+    cat > ${SERVICE_FILE} <<-EOF
+    [Unit]
+    Description=works as a relay to retransmit poses at a specific frequency
+    After=network.target
 
-# Enable the service to start at boot
-systemctl enable "${seed_s7}.service"
+    [Service]
+    ExecStartPre=/bin/sleep 3 # delay 3 seconds
+    ExecStart=/usr/bin/python3 /home/${USER}/rokit/service/${target} --frq_divisor 3 --src_host 127.0.0.1 --src_port 9011 --dst_port 9511
+    Restart=always
 
-# Start the service
-systemctl start "${seed_s7}.service"
+    [Install]
+    WantedBy=multi-user.target
+EOF
+    
+    echo "======${SERVICE_FILE}"
+    cat "${SERVICE_FILE}"
+    echo "======"
+    
+    # Reload systemd daemon to recognize new service file
+    systemctl daemon-reload
+    
+    # Enable the service to start at boot
+    systemctl enable "${target}.service"
+    
+    # Start the service
+    systemctl start "${target}.service"
+    
+    echo "Successfully set up ${target} as a systemd service."
+}
 
-echo "Successfully set up ${seed_s7} as a systemd service."
+
+# ANSI escape codes for colors
+RED='\033[0;31m'
+COLOR="\033[43;42m"
+GREEN_B="\e[42m" # https://gist.github.com/Prakasaka/219fe5695beeb4d6311583e79933a009
+YELLOW_F='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+start_menu(){
+    # clear
+    echo
+    printf "${COLOR}====================================${NC}\n"
+    printf "${COLOR}%-10s%-26s${NC}\n" "Script" $0
+    printf "${COLOR}%-10s%-26s${NC}\n" "OS" "$(lsb_release -ds)"
+    printf "${COLOR}%-10s%-26s${NC}\n" "Author" "TAN Hongkui"
+    printf "${GREEN_B}====================================${NC}\n"
+    echo
+    echo -e "${YELLOW_F} 1. 安装seed_s7${NC}"
+    echo -e "${YELLOW_F} 2. 安装relay.py${NC}"
+    echo -e "${YELLOW_F} 3. 安装simple_vanjee_716mini${NC}"
+    echo -e "${YELLOW_F} 4. 安装seed_modbus${NC}"
+    echo -e " 0. ctrl+c退出脚本"
+    echo
+    read -p "请输入数字:" num
+    case "$num" in
+        1)
+            install_seed_s7
+        ;;
+        2)
+            install_relay
+        ;;
+        3)
+            install_simple_vanjee_716mini
+        ;;
+        4)
+            install_seed_modbus
+        ;;
+        0)
+            exit 1
+        ;;
+        *)
+            # clear
+            echo -e "请输入正确数字"
+            sleep 2s
+            start_menu
+        ;;
+    esac
+}
+
+while true; do
+    start_menu
+    sleep 2s
+    if [ $? -ne 0 ]
+    then
+        break
+    fi
+done
